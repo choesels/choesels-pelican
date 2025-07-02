450edaebfc0583c18f8a0d28701607e399745cf8
ulf gjerdignen <pagehead@gmail.com>
2025-07-02 11:08:00 +0200

    add display latest poster logic to concert section

--- pelicanconf.py
+++ pelicanconf.py
@@ -47,22 +47,22 @@ DEFAULT_PAGINATION = False
 PAGE_PATHS = ['pages']
 
 CONCERTS = [
-    {"title": "Summer Fest 1998", "date": "1998-06-20", "location": "Central Park, NYC, USA"},
-    {"title": "Bar Mitzva for Ezekiel 1", "date": "2003-03-01", "location": "Sablon, Bxl"},
-    {"title": "Bar Mitzva for Ezekiel 2", "date": "2003-03-01", "location": "Sablon, Bxl"},
-    {"title": "Bar Mitzva for Ezekiel 3", "date": "2003-03-01", "location": "Sablon, Bxl"},
-    {"title": "Autumn Jam", "date": "2004-10-05", "location": "Downtown Hall, Uptown"},
-    {"title": "", "date": "2024-03-23", "location": "Cafe Terminus, Evere"},
-    {"title": "", "date": "2024-05-04", "location": "Le Fille de on Pere, Rebecq"},
-    {"title": "CANCELLED", "date": "2024-06-22", "location": "??"},
-    {"title": "", "date": "2024-07-27", "location": "l'Exelcior, Jette"},
-    {"title": "", "date": "2024-09-14", "location": "Madras Bar, Braine-l'Alleud"},
-    {"title": "", "date": "2024-09-28", "location": "Cafe Terminus, Evere"},
-    {"title": "", "date": "2024-12-21", "location": "Le Métropole, Braine-l'Alleud"},
-    {"title": "Bar Mitzva for Ezekiel 1", "date": "2028-02-01", "location": "Sablon, Bxl"},
-    {"title": "Bar with Beers for Ezekiel 2", "date": "2028-03-01", "location": "Sablon, Bxl"},
-    {"title": "Barbapapa for Kids", "date": "2028-04-01", "location": "Sablon, Bxl"},
-    {"title": "Bar for Me", "date": "2029-01-01", "location": "Sablon, Bxl"},
+    {"title": "Summer Fest 1998", "date": "1998-06-20", "location": "Central Park, NYC, USA", "event_link": ""},
+    {"title": "Bar Mitzva for Ezekiel 1", "date": "2003-03-01", "location": "Sablon, Bxl", "event_link": ""},
+    {"title": "Bar Mitzva for Ezekiel 2", "date": "2003-03-01", "location": "Sablon, Bxl", "event_link": ""},
+    {"title": "Bar Mitzva for Ezekiel 3", "date": "2003-03-01", "location": "Sablon, Bxl", "event_link": ""},
+    {"title": "Autumn Jam", "date": "2004-10-05", "location": "Downtown Hall, Uptown", "event_link": ""},
+    {"title": "", "date": "2024-03-23", "location": "Cafe Terminus, Evere", "event_link": "https://www.facebook.com/events/8-rue-%C3%A9glise-st-pierre-jette-belgique/the-choesels/457359800386869/"},
+    {"title": "", "date": "2024-05-04", "location": "Le Fille de on Pere, Rebecq", "event_link": ""},
+    {"title": "CANCELLED", "date": "2024-06-22", "location": "??", "event_link": ""},
+    {"title": "", "date": "2024-07-27", "location": "l'Exelcior, Jette", "event_link": ""},
+    {"title": "", "date": "2024-09-14", "location": "Madras Bar, Braine-l'Alleud", "event_link": ""},
+    {"title": "", "date": "2024-09-28", "location": "Cafe Terminus, Evere", "event_link": ""},
+    {"title": "", "date": "2024-12-21", "location": "Le Métropole, Braine-l'Alleud", "event_link": ""},
+    {"title": "Bar Mitzva for Ezekiel 1", "date": "2028-02-01", "location": "Sablon, Bxl", "event_link": ""},
+    {"title": "Bar with Beers for Ezekiel 2", "date": "2028-03-01", "location": "Sablon, Bxl", "event_link": ""},
+    {"title": "Barbapapa for Kids", "date": "2028-04-01", "location": "Sablon, Bxl", "event_link": ""},
+    {"title": "Bar for Me", "date": "2029-01-01", "location": "Sablon, Bxl", "event_link": ""},
 ]
 
 from datetime import datetime, date
@@ -74,8 +74,29 @@ JINJA_FILTERS = {
     'as_date': as_date,
 }
 
+import os
+import re
+
+# poster image path
+folder = 'theme/static/img/posters'
+
+# Regex to extract date in format YYYY-MM-DD (e.g., 2028-01-01)
+date_pattern = re.compile(r'(\d{4}-\d{2}-\d{2})')
+latest_file = None
+latest_date = None
+for filename in os.listdir(folder):
+    match = date_pattern.search(filename)
+    if match:
+        # Parse the date from filename
+        file_date = datetime.strptime(match.group(1), "%Y-%m-%d")
+        # Compare to current latest
+        if latest_date is None or file_date > latest_date:
+            latest_date = file_date
+            latest_file = filename
+
 JINJA_GLOBALS = {
     'concerts': CONCERTS,
     'date': date,
+    'latest_poster': latest_file,
+    'latest_poster_date': latest_date.date(),
 }
-
